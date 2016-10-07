bdb54b5f3c3f201bd6d0fe577d37cada17c737a1
Rafael Ivan Garcia <rafaelivan@Rafaels-MacBook-Pro-2.local>
2016-10-07 11:33:03 -0300

    Adds set_env_variables.rb

---
+++ config/initializers/set_env_variables.rb
@@ -0,0 +1,14 @@
+# -----------------------------------------------------------------------------
+# Please, make sure you add to config/boot.rb in your Rails app:
+# require File.expand_path('../initializers/set_env_variables', __FILE__)
+
+# GENERAL
+ENV['SECRET_KEY_BASE'] = 'INSIRA_AQUI_A_SECRET_KEY_BASE'
+
+# MYSQL
+ENV['MD5_GENERATOR_PG_HOST'] = 'INSIRA_AQUI_O_ENDPOINT_DO_RDS'
+ENV['MD5_GENERATOR_PG_PORT'] = 'INSIRA_AQUI_A_PORTA_DO_RDS'
+ENV['MD5_GENERATOR_PG_DATABASE'] = 'INSIRA_AQUI_O_NOME_DO_BANCO_DE_DADOS'
+ENV['MD5_GENERATOR_PG_USERNAME'] = 'INSIRA_AQUI_O_NOME_DE_USUARIO_DO_RDS'
+ENV['MD5_GENERATOR_PG_PASSWORD'] = 'INSIRA_AQUI_A_SENHA_DO_RDS'
+# -----------------------------------------------------------------------------
